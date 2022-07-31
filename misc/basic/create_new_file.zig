d0a33331639541399113109d1f279939a56e2b78
Kotaro Inoue <k.musaino@gmail.com>
2022-07-31 18:23:52 +0900

    Add create_new_file sample
    Signed-off-by: Kotaro Inoue <k.musaino@gmail.com>

---
+++ misc/basic/create_new_file.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+const fs = std.fs;
+const testing = std.testing;
+const ArenaAllocator = std.heap.ArenaAllocator;
+const print = std.debug.print;
+
+pub fn main() !void {
+    var arena = ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    // https://github.com/ziglang/zig/blob/6d44a6222d6eba600deb7f16c124bfa30628fb60/lib/std/fs/test.zig#L1031
+    const base_path = blk: { // I don't know why this is needed
+        const relative_path = try fs.path.join(allocator, &[_][]const u8{"."});
+        break :blk try fs.realpathAlloc(allocator, relative_path);
+    };
+
+    const message = "Hello, world!\n";
+    // if you want to create another subdir, use fs.path.join
+    // `var subdir_path = try fs.path.join(allocator, &[_][]const u8{ base_path, "hoge" });`
+    fs.makeDirAbsolute(base_path) catch |err| switch (err) {
+        error.PathAlreadyExists => {},
+        else => return err,
+    };
+    try fs.accessAbsolute(base_path, .{});
+    var subdir = try fs.openDirAbsolute(base_path, .{});
+    subdir.writeFile("test.txt", message) catch |err| switch (err) {
+        error.PathAlreadyExists => {},
+        else => return err,
+    };
+}
