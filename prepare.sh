e29d2fff21d42c4ad55b5b3f81496f0a4f40da81
Kotaro Inoue <k.musaino@gmail.com>
2022-08-10 02:23:30 +0900

    Add prepare script for pivot_root
    Signed-off-by: Kotaro Inoue <k.musaino@gmail.com>

---
+++ prepare.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eu
+
+mkdir -m 755 -p /root/rootfs/proc
+mkdir -m 755 -p /root/rootfs/bin
+mkdir -m 755 -p /root/rootfs/lib
+
+cp -Lr /bin/sh /root/rootfs/bin
+cp -Lr /bin/ls /root/rootfs/bin
+cp -Lr /usr/bin/id /root/rootfs/bin
+
+cp -Lr /lib/x86_64-linux-gnu /root/rootfs/lib
+cp -Lr /lib64/ld-linux-x86-64.so.2 /root/rootfs/lib
+
+cd /root/rootfs/
+ln -s lib lib64
