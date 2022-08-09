889e666f46d7f0af502a6226913c6b462b5ae45f
Kotaro Inoue <k.musaino@gmail.com>
2022-08-10 02:27:39 +0900

    Fix prepare.sh
    Signed-off-by: Kotaro Inoue <k.musaino@gmail.com>

--- prepare.sh
+++ prepare.sh
@@ -6,9 +6,8 @@ mkdir -m 755 -p /root/rootfs/proc
 mkdir -m 755 -p /root/rootfs/bin
 mkdir -m 755 -p /root/rootfs/lib
 
-cp -Lr /bin/sh /root/rootfs/bin
-cp -Lr /bin/ls /root/rootfs/bin
-cp -Lr /usr/bin/id /root/rootfs/bin
+cp -Lr /bin/* /root/rootfs/bin
+cp -Lr /usr/bin/* /root/rootfs/bin
 
 cp -Lr /lib/x86_64-linux-gnu /root/rootfs/lib
 cp -Lr /lib64/ld-linux-x86-64.so.2 /root/rootfs/lib
