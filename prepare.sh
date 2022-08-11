f9281e2e57c2e06f78f6fee563441164813355f4
Kotaro Inoue <k.musaino@gmail.com>
2022-08-12 00:24:59 +0900

    Fix prepare.sh arch-independent
    Signed-off-by: Kotaro Inoue <k.musaino@gmail.com>

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+arch=$(uname -m)
+
 mkdir -m 755 -p /root/rootfs/proc
 mkdir -m 755 -p /root/rootfs/bin
 mkdir -m 755 -p /root/rootfs/lib
@@ -9,8 +11,9 @@ mkdir -m 755 -p /root/rootfs/lib
 cp -Lr /bin/* /root/rootfs/bin
 cp -Lr /usr/bin/* /root/rootfs/bin
 
-cp -Lr /lib/x86_64-linux-gnu /root/rootfs/lib
-cp -Lr /lib64/ld-linux-x86-64.so.2 /root/rootfs/lib
+cp -Lr /lib/${arch}-linux-gnu /root/rootfs/lib
+[ -e /lib/ld-linux-${arch}.so.2 ] && cp -Lr /lib/ld-linux-${arch}.so.2 /root/rootfs/lib
+[ -e /lib64/ld-linux-${arch}.so.2 ] && cp -Lr /lib64/ld-linux-${arch}.so.2 /root/rootfs/lib
 
 cd /root/rootfs/
 ln -s lib lib64
